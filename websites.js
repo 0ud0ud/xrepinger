4b8872fbbea79c4403f81bf0ce4f4e09c217f380
Alexandre Leguay <alexandre.leguay@flashtalking.com>
2017-04-19 13:16:49 +0100

    change timeout

--- websites.js
+++ websites.js
@@ -3,91 +3,91 @@
 */
 module.exports = [{
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;gif;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;gifimpid;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;guid;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;html;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;iframe;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;jpg;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;js;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;jsappend;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;jsiframe;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;noscript;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;noscriptstate;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;pixel;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;png;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/62837;1850446;201;redirect;testingcom;Rule1Singlecreative/?cachebuster=558846.2938840&url=http://google.com/',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/imp/8/61062;2429268;208;xml;testingcom;a56VASTEarthspin/?cachebuster=5338411995',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/map/?key=value&url=http://www.google.com/?g=[%FT_GUID%]',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/click/1/62837;1850446;1368701;210;0/?ft_impID=3065EFA17D3CDC&g=30395833B24565&random=93440267&ft_width=300&ft_height=250&url=http://www.google.com/',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/click/1/62837;1850446;1368701;210;0/?ft_impID=3065EFA17D3CDC&g=30395833B24565&random=93440267&ft_width=300&ft_height=250&url=5461791',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/container/8342;65021;6827;iframe/?spotName=HTTP_iframe_&cachebuster=145649.78656',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/container/8342;65021;6827;js/?spotName=HTTP_js_&cachebuster=145649.78656',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/spot/1/8342;65021;6827/?spotName=HTTP_iframe_&cachebuster=679680.8667375629&ft_guid=3348C8D67AFCAB',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/dynamic/oba_us/adChoicesUS_obaurllink.html',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/segment/modify/aaa;;click/?name=test&cb=123&url=http://www.google.com/',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/segment/modify/aaa;;pixel/?name=test&cb=123',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/segment/modify/aaa;;text/?name=test&cb=123',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/segment/modify/aaa;;javascript/?name=test&cb=123',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/state/1953764;441861;0;273;321463569586EB/?cachebuster=123&url=http://www.google.com/',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/state/1953764;441861;0;273;321463569586EB/?cachebuster=123',
-    timeout: 0.2
+    timeout: 60
 }, {
     url: 'http://servedby.flashtalking.com/track/12345;1234;202;31843D60889BCA/?cachebuster=123',
-    timeout: 0.2
+    timeout: 60
 }];
 
 
